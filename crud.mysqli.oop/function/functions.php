f7893d13767c0f9c545c37a387290f63b8001eda
Josh Lasac <100702615+joshlsc@users.noreply.github.com>
2023-02-15 13:59:53 +0800

    Update functions.php

--- crud.mysqli.oop/function/functions.php
+++ crud.mysqli.oop/function/functions.php
@@ -32,32 +32,3 @@ function deleteMsg(){
              ';
     }
 }
-
-function notifyMsg(){
-    if(isset($_GET['success'])){
-        echo '
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    <strong>Great!</strong> New record inserted successfully.
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-             ';
-    }
-
-    if(isset($_GET['updated'])){
-        echo '
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    <strong>Alright!</strong> Record updated successfully.
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-             ';
-    }
-
-    if(isset($_GET['deleted'])){
-        echo '
-                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    <strong>Notice:</strong> Record deleted successfully.
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-             ';
-    }
-}
